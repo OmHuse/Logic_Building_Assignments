f8b01cb9816c4e681ebdc95d726c410509bfb6e6
OmHuse <sohamhuse@gmail.com>
2025-05-09 13:20:26 +0530

    Add files via upload

---
+++ Assignment_1/Program3.c
@@ -0,0 +1,35 @@
+///////////////////////////////////////////////////////////////////////
+//
+//     File Name    : Program3.c
+//
+//     Description  : Program to print 5 to 1 numbers on screen 
+//
+//     Author       : Om Laxman Huse
+//
+//     Date         : 08/05/2025
+//     
+////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+
+#include <stdio.h>
+
+void Display()
+ {
+    int i = 0;
+
+    i = 5;
+
+    while (i >= 1)
+     {
+        printf("%d ", i);
+        i--;
+    }
+}
+
+int main()
+ {
+    Display();
+    return 0;
+}
+
+
---
+++ Assignment_1/Program5.c
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////////////////////////
+//
+//     File Name    : Program5.c
+//
+//     Description  : Aceept one number form user and print that number of * on screen
+//
+//     Author       : Om Laxman Huse
+//
+//     Date         : 08/05/2025
+//     
+///////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+ 
+
+   void Accept(int iNo)
+   {
+        int iCnt = 0;
+
+        for (iCnt==1;    ;   )
+        {
+            printf("*");
+        }
+   }
+
+   
+    int main ()
+    {   
+        int iValue = 0;
+        iValue = 5;
+
+        Accept(iValue);
+        return 0;
+    }
